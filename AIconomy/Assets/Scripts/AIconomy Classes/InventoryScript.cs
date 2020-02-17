10fef004f537b225dd6654bcdf74d72d12ffd81c
Ben Strong <benjamin.strong@mymail.champlain.edu>
2020-02-17 15:27:45 -0500

    changed preferred external IDE.  Created UML diagram (on google drive)

--- AIconomy/Assets/Scripts/AIconomy Classes/InventoryScript.cs
+++ AIconomy/Assets/Scripts/AIconomy Classes/InventoryScript.cs
@@ -6,6 +6,7 @@ public class InventoryScript : MonoBehaviour
 {
     public int walletAmount;
     public List<GameObject> items = new List<GameObject>();
+    //public FactionTypes.faction alliedFaction;
 
     public int getWalletAmount()
     {
