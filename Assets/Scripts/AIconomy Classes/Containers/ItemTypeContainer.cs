847b72e7ad45fd32822143c4266c780c08509a08
Ben Strong <benjamin.strong@mymail.champlain.edu>
2020-02-26 12:17:17 -0500

    Did more work

---
+++ Assets/Scripts/AIconomy Classes/Containers/ItemTypeContainer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemTypeContainer : MonoBehaviour
+{
+    [SerializeField]
+    ItemType[] itemTypes
+
+    public int foo;
+
+    public void addItem(string name)
+    {
+        if(itemTypes == null)
+        {
+            Debug.Log("itemTypes is null");
+            itemTypes = new List<ItemType>();
+        }
+
+        Debug.Log("Item Added: " + name);
+        ItemType item;
+        foo++;
+        item.iName = name;
+        itemTypes.Add(item);
+    }
+
+    private void Start()
+    {
+        Debug.Log(itemTypes);
+    }
+}
